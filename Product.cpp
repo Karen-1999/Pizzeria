a56fd1cff07c81441d48968361dea69c5bdc48c7
Karen <efremyan.kk@phystech.edu>
2018-03-19 16:07:55 +0300

    Fixed mutable fields

--- Product.cpp
+++ Product.cpp
@@ -15,8 +15,8 @@ void Pizza::SetSpecies(const std::string p) { pspecies = p; }
 void Pizza::Status()
 {
     std::cout << "   Dear visitor !" << std::endl
-         << "Pizza size is " << psize
-         << ", sauce is " << psauce
-         << " and species are " << pspecies
+         << "Pizza size is " << GetSize()
+         << ", sauce is " << GetSauce()
+         << " and species are " << GetSpecies()
          << " ! " << std::endl << "   Bon apetite !!!"<<std::endl;
 }
